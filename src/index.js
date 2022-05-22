cd094354231f100c027ae6b85a5e3378ac805fd4
unknown <C:\Users\hp\AppData\Roaming\The Bat!>
2022-05-22 09:32:06 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    let elements = document.querySelectorAll('ul > li:nth-child(odd)')
+    for (let elem of elements) {
+        elem.style.backgroundcolor = 'red'
+    }
+};
 
 export const findElement = () => {};
