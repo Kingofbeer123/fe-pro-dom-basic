ec645cc0abc5a0aab301f340bcdb61600254daf7
unknown <C:\Users\hp\AppData\Roaming\The Bat!>
2022-05-22 09:54:02 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export const paintCards = () => {
-    const elements = document.querySelectorAll('.item:nth-child(odd)');
+    const elements = document.querySelectorAll(".item:nth-child(odd)");
     for (let elem of elements) {
-        elem.style.backgroundcolor = 'red';
+        elem.style.backgroundcolor = "red";
     }
 };
 
