f70a72f140b6ae314813b73ef8a6f4c8a775d5f6
unknown <C:\Users\hp\AppData\Roaming\The Bat!>
2022-05-22 10:23:52 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 export const paintCards = () => {
-    const elements = document.querySelectorAll(".item:nth-child(odd)");
+    let elements = document.querySelectorAll('.item:nth-child(odd)');
     for (let elem of elements) {
-        elem.style.backgroundcolor = "red";
+        elem.style.backgroundcolor = 'red';
     }
 };
 
-export const findElement = () => {};
+export const findElement = () => {
+    const elements = document.querySelectorAll('.item');
+    let elem = document.querySelectorAll('item:first-child');
+    for (let a = 0; a < elements.length; a++) {
+        if (elem.matches('.lickedItem')) {
+            elem.style.backgroundcolor = 'blue';
+        }
+        else {
+            elem = elem.nextElementSibling;
+        }
+    }
+};
