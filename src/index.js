cd220200c4e282985953c7c3fdf7d094a4880e8c
unknown <C:\Users\hp\AppData\Roaming\The Bat!>
2022-05-22 10:31:50 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export const paintCards = () => {
-    let elements = document.querySelectorAll(".item:nth-child(odd)");
+    const elements = document.querySelectorAll(".item:nth-child(odd)");
     for (let elem of elements) {
         elem.style.backgroundcolor = "red";
     }
